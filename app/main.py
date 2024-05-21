202b112de6939bff7fa2268e133e89bf89ecd408
Dillon Troxell <Dillon.Troxell@8451.com>
2024-05-21 15:09:52 -0400

    pass stage 5

--- app/main.py
+++ app/main.py
@@ -12,8 +12,11 @@ def main():
     (conn, addr) = server_socket.accept()  # wait for client
     with conn:
         print(f"Connected by {addr}")
+        # data = bytes object
         data = conn.recv(1024)
+        # data_str = str
         data_str = data.decode()
+        # METHOD /path <-- We get path
         path = data_str.split(" ")[1]
         if path == "/":
             conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
@@ -23,9 +26,15 @@ def main():
             if(wordToEcho):
                 echoBytes = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(wordToEcho)}\r\n\r\n{wordToEcho}".encode()
             conn.send(echoBytes)
+        elif path == "/user-agent":
+            userAgent = (data_str.split('User-Agent:')[1].split('\r\n')[0]).strip()
+            userAgentBytes = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(userAgent)}\r\n\r\n{userAgent}".encode()
+            conn.send(userAgentBytes)
         else:
             conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        print("Closing Client Connection...")
         conn.close()
+    print("Closing Server Connection...")
     server_socket.close()
 
 
