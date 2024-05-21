8aa5424e8a6d8701af7393ebe9182819842429d1
Dillon Troxell <Dillon.Troxell@8451.com>
2024-05-21 13:13:10 -0400

    pass stage 3

--- app/main.py
+++ app/main.py
@@ -10,8 +10,17 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     (conn, addr) = server_socket.accept() # wait for client
-    if(conn):
-        conn.send(b'HTTP/1.1 200 OK\r\n\r\n')
+    with conn:
+        print(f'Connected by {addr}')
+        data = conn.recv(1024)
+        data_str = data.decode()
+        url = data_str.split(' ')[1]
+        if(url != "/"):
+            conn.send(b'HTTP/1.1 404 Not Found\r\n\r\n')
+        else:
+            conn.send(b'HTTP/1.1 200 OK\r\n\r\n')
+        conn.close()
+    server_socket.close()
 
 if __name__ == "__main__":
     main()
